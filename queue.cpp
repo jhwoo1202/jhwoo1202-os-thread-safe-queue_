c6a2f74c2d55600bfbe40883c96e59954d2a5208
jhwoo1202 <165012321+jhwoo1202@users.noreply.github.com>
2025-06-05 12:29:06 +0900

    queue.cpp (1-4-1) 중복 제거
    Init(void)함수 중복 제거

--- queue.cpp
+++ queue.cpp
@@ -3,15 +3,6 @@
 #include <cstring>
 #include <mutex>
 
-Queue* init(void) {
-    Queue* q = (Queue*)std::malloc(sizeof(Queue));
-    if (!q) return nullptr;
-    q->head = nullptr;
-    q->tail = nullptr;
-
-    return q;
-}
-
 Node* nalloc(Item item) {
     Node* node = (Node*)std::malloc(sizeof(Node));
     if (!node) return nullptr;
