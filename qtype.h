efc9f0525af19f4418573683e2f8695d530289fe
jhwoo1202 <165012321+jhwoo1202@users.noreply.github.com>
2025-06-05 10:11:45 +0900

    qtype.h enqueue, dequeue, range함수 프로토타입 수정

--- qtype.h
+++ qtype.h
@@ -31,3 +31,7 @@ typedef struct {
 Node*  nalloc(Item item);
 void   nfree(Node* node);
 Node*  nclone(Node* node);
+
+Reply  enqueue(Queue* queue, Item item);
+Reply  dequeue(Queue* queue);
+Queue* range(Queue* queue, Key start, Key end);
